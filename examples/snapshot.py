e1f9f7bff56cb4dcc5d9b80cfa196644be994c99
Maximilian Mehnert <maximilian.mehnert@gmx.de>
2016-02-10 12:38:16 +0100

    iterate over pool and deliver fs with clones first, retabbing, cleanup

--- examples/snapshot.py
+++ examples/snapshot.py
@@ -12,40 +12,40 @@ import sys
 from zfs_functions import *
 
 if __name__ == '__main__':
-  parser=argparse.ArgumentParser(description="take some snapshots")
-  parser.add_argument("fs", help="The zfs filesystem to act upon")
-  parser.add_argument("prefix",help="The prefix used to name the snapshot(s)")
-  parser.add_argument("-r",help="Recursively take snapshots",action="store_true")
-  parser.add_argument("-k",type=int, metavar="n",help="Keep n older snapshots with same prefix. Otherwise delete none.")
-  parser.add_argument("--dry-run", help="Just display what would be done. Notice that since no snapshots will be created, less will be marked for theoretical destruction. ", action="store_true")
-  parser.add_argument("--verbose", help="Display what is being done", action="store_true")
-  parser.add_argument("--remote", help="e.g. \"ssh hostname\"", default="")
+	parser=argparse.ArgumentParser(description="take some snapshots")
+	parser.add_argument("fs", help="The zfs filesystem to act upon")
+	parser.add_argument("prefix",help="The prefix used to name the snapshot(s)")
+	parser.add_argument("-r",help="Recursively take snapshots",action="store_true")
+	parser.add_argument("-k",type=int, metavar="n",help="Keep n older snapshots with same prefix. Otherwise delete none.")
+	parser.add_argument("--dry-run", help="Just display what would be done. Notice that since no snapshots will be created, less will be marked for theoretical destruction. ", action="store_true")
+	parser.add_argument("--verbose", help="Display what is being done", action="store_true")
+	parser.add_argument("--remote", help="e.g. \"ssh hostname\"", default="")
 
-  args=parser.parse_args()
-  print args
+	args=parser.parse_args()
+	print args
 
-  try:
-    pool=ZFS_pool(pool=args.fs.split("/")[0],remote_cmd=args.remote)
-  except subprocess.CalledProcessError, e:
-    sys.exit()
-  fs_obj=ZFS_fs(fs=args.fs, pool=pool)
-  if args.r==False:
-    create_zfs_snapshot(fs=fs_obj,prefix=args.prefix,dry_run=args.dry_run, verbose=args.verbose)
-    if args.k != None and args.k >= 0:
-      #if we are here, the snapshot was created. We did not update, so subtract 1 from args.k
-      clean_zfs_snapshots(fs=fs_obj, prefix=args.prefix,
-                  number_to_keep=args.k-1,dry_run=args.dry_run,
-                  verbose=args.verbose)
+	try:
+		pool=ZFS_pool(pool=args.fs.split("/")[0],remote_cmd=args.remote)
+	except subprocess.CalledProcessError, e:
+		sys.exit()
+	fs_obj=ZFS_fs(fs=args.fs, pool=pool)
+	if args.r==False:
+		create_zfs_snapshot(fs=fs_obj,prefix=args.prefix,dry_run=args.dry_run, verbose=args.verbose)
+		if args.k != None and args.k >= 0:
+			#if we are here, the snapshot was created. We did not update, so subtract 1 from args.k
+			clean_zfs_snapshots(fs=fs_obj, prefix=args.prefix,
+				number_to_keep=args.k-1,dry_run=args.dry_run,
+				verbose=args.verbose)
 
-  else:
-    for fs in pool.get_zfs_filesystems(fs=fs_obj.fs):
-      fs=ZFS_fs(fs=fs,pool=pool)
-      create_zfs_snapshot(fs=fs,
-                          prefix=args.prefix,dry_run=args.dry_run,
-                          verbose=args.verbose)
-      if args.k != None and args.k >= 0:
-        clean_zfs_snapshots(fs=fs, prefix=args.prefix,
-                    number_to_keep=args.k-1,dry_run=args.dry_run,
-                    verbose=args.verbose)
+	else:
+		for fs in pool.get_zfs_filesystems(fs=fs_obj.fs):
+			fs=ZFS_fs(fs=fs,pool=pool)
+			create_zfs_snapshot(fs=fs,
+				prefix=args.prefix,dry_run=args.dry_run,
+				verbose=args.verbose)
+			if args.k != None and args.k >= 0:
+				clean_zfs_snapshots(fs=fs, prefix=args.prefix,
+					number_to_keep=args.k-1,dry_run=args.dry_run,
+					verbose=args.verbose)
 
 
--- examples/transfer.py
+++ examples/transfer.py
@@ -5,47 +5,47 @@ Created on 3 Sep 2012
 @author: Maximilian Mehnert <maximilian.mehnert@gmx.de
 '''
 
-from  zfs_functions import *
+from	zfs_functions import *
 
 
 
 if __name__ == '__main__':
-  transfer_marker_prefix="backup"
-  dry_run=False
-  # we are transferring / synchronizing the pool all from 192.168.50.20 to the local machine
-  src_pool=ZFS_pool(pool="all", remote_cmd="ssh 192.168.50.20")
-  dst_pool=ZFS_pool(pool="all")
-  # the copy of the remote pool "all" will end up in "all/backup/all" on the local machine
-  dst_prefix="all/backup/"
-  
-  for fs in src_pool.zfs_filesystems: 
-    print fs
-    src_fs=ZFS_fs(fs=fs, pool=src_pool)    
-    dst_fs=ZFS_fs(fs=dst_prefix+fs,pool=dst_pool)
+	transfer_marker_prefix="backup"
+	dry_run=False
+	# we are transferring / synchronizing the pool all from 192.168.50.20 to the local machine
+	src_pool=ZFS_pool(pool="all", remote_cmd="ssh 192.168.50.20")
+	dst_pool=ZFS_pool(pool="all")
+	# the copy of the remote pool "all" will end up in "all/backup/all" on the local machine
+	dst_prefix="all/backup/"
 
-    if not sync_zfs_fs(src_fs=src_fs, dst_fs=dst_fs,target_name=transfer_marker_prefix,
-                       verbose=True, dry_run=dry_run):
-      print "sync failure for "+fs
-    else:
-      # here we are deleting older snapshots starting with transfer_marker_prefix, in this case keeping
-      # the youngest three
-      clean_zfs_snapshots(src_fs, prefix=transfer_marker_prefix,
-                           number_to_keep=3, dry_run=dry_run, verbose=True)
-      clean_zfs_snapshots(dst_fs, prefix=transfer_marker_prefix,
-                           number_to_keep=3, dry_run=dry_run, verbose=True)
-      # on the destination pool (in this case local machine, see above), clean up snapshots starting with the prefixes below,
-      # keeping the youngest n according to the second part of the tuples
-      for tuple in [["5min",1],
-                    ["hourly",1],
-                    ["quarterly",1],
-                    ["daily",7],
-                    ["weekly",4],
-                    ["monthly",12]]:
-        clean_zfs_snapshots(fs=dst_fs, prefix=tuple[0],
-                      number_to_keep=tuple[1],dry_run=dry_run, verbose=True)      
-      # now we clean up unrelated snapshots both on the remote and local machine, ignoring those starting with the prefixes
-      # configured below, keeping the 10 youngest
-      prefixes_to_ignore=["hourly","weekly","quarterly","daily","weekly","monthly","yearly",transfer_marker_prefix]
-      clean_other_zfs_snapshots(fs=dst_fs, prefixes_to_ignore=prefixes_to_ignore,number_to_keep=10, dry_run=dry_run, verbose=verbose)
-      clean_other_zfs_snapshots(fs=src_fs, prefixes_to_ignore=prefixes_to_ignore,number_to_keep=10, dry_run=dry_run, verbose=verbose)
+	for fs in src_pool:
+		print fs
+		src_fs=ZFS_fs(fs=fs, pool=src_pool)
+		dst_fs=ZFS_fs(fs=dst_prefix+fs,pool=dst_pool)
+
+		if not sync_zfs_fs(src_fs=src_fs, dst_fs=dst_fs,target_name=transfer_marker_prefix,
+											 verbose=True, dry_run=dry_run):
+			print "sync failure for "+fs
+		else:
+			# here we are deleting older snapshots starting with transfer_marker_prefix, in this case keeping
+			# the youngest three
+			clean_zfs_snapshots(src_fs, prefix=transfer_marker_prefix,
+													 number_to_keep=3, dry_run=dry_run, verbose=True)
+			clean_zfs_snapshots(dst_fs, prefix=transfer_marker_prefix,
+													 number_to_keep=3, dry_run=dry_run, verbose=True)
+			# on the destination pool (in this case local machine, see above), clean up snapshots starting with the prefixes below,
+			# keeping the youngest n according to the second part of the tuples
+			for tuple in [["5min",1],
+										["hourly",1],
+										["quarterly",1],
+										["daily",7],
+										["weekly",4],
+										["monthly",12]]:
+				clean_zfs_snapshots(fs=dst_fs, prefix=tuple[0],
+											number_to_keep=tuple[1],dry_run=dry_run, verbose=True)
+			# now we clean up unrelated snapshots both on the remote and local machine, ignoring those starting with the prefixes
+			# configured below, keeping the 10 youngest
+			prefixes_to_ignore=["hourly","weekly","quarterly","daily","weekly","monthly","yearly",transfer_marker_prefix]
+			clean_other_zfs_snapshots(fs=dst_fs, prefixes_to_ignore=prefixes_to_ignore,number_to_keep=10, dry_run=dry_run, verbose=verbose)
+			clean_other_zfs_snapshots(fs=src_fs, prefixes_to_ignore=prefixes_to_ignore,number_to_keep=10, dry_run=dry_run, verbose=verbose)
 
